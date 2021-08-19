33910beaa7dee4f642e44e92d41e8464ca900bbc
Jacob Chvatal <jake@isnt.online>
2021-08-19 13:18:59 -0700

    Add 404

---
+++ src/pages/404.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+// Source: Primarily https://github.com/rasendubi/www.alexeyshmalko.com/blob/master/pages/uniorg.tsx
+
+export default function Page404() {
+  return (
+    <div className="root">
+      <h1>{"404"}</h1>
+      <p>{"This page is private or does not exist."}</p>
+      <p>
+        {"Go to:"}
+        <ul>
+          <li>
+            <Link href="/">{"Home page"}</Link>
+          </li>
+          <li>
+            <Link href="/archive">{"Search"}</Link>
+          </li>
+        </ul>
+      </p>
+
+      <style jsx>{`
+        .root {
+          flex-grow: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        h1 {
+          margin-bottom: 4px;
+        }
+        p {
+          text-align: center;
+        }
+        ul {
+          padding: 0;
+        }
+      `}</style>
+    </div>
+  );
+}
